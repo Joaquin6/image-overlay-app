5456594e863728a0801df520ab0e771d2c77017d
joaquin.briceno <joaquin_briceno@yahoo.com>
2017-03-04 13:23:12 -0800

    added toolbar save img functionality

--- js/main.js
+++ js/main.js
@@ -6,8 +6,30 @@ var editor = document.getElementById("overlayEditor"),
 	context = editor.getContext("2d"),
 	/** create/load image */
 	image = $("<img/>", {
-		src: "img/graffiti.png",
+		src: "images/graffiti.png",
 		load: function() {
 			context.drawImage(this, 0, 0);
 		}
 	});
+
+/** toolbar functions  */
+var tools = {
+	save: function() {
+	    var saveDialog = $("<div>").appendTo("body");
+	    $("<img/>", {
+	        src: editor.toDataURL()
+	    }).appendTo(saveDialog);
+	    saveDialog.dialog({
+	        resizable: false,
+	        modal: true,
+	        title: "Right-click and choose 'Save Image As'",
+	        width: editor.width + 35
+	    });
+	}
+};
+
+$("#editorToolbar").children().click(function(e) {
+    e.preventDefault();
+    /** call the relevant function */
+    tools[this.id].call(this);
+});
