4d140982051d5fc146150695a2f9d142b51b3740
joaquin.briceno <joaquin_briceno@yahoo.com>
2017-03-04 13:53:50 -0800

    added resize functionality

--- js/main.js
+++ js/main.js
@@ -67,6 +67,90 @@ var tools = {
 	        r: 90 * Math.PI / 180
 	    };
 	    tools.rotate(conf);
+	},
+	/**
+	 * Resizing Images
+	 * This is the most complex interaction,
+	 * and the function required to do it is quite large,
+	 * even though resizing the canvas itself is quite trivial
+	 */
+	resize: function() {
+	    var coords = $(editor).offset(),
+	        resizer = $("<div>", {
+	            id: "resizer"
+	        }).css({
+	            position: "absolute",
+	            left: coords.left,
+	            top: coords.top,
+	            width: editor.width - 1,
+	            height: editor.height - 1
+	        }).appendTo("body");
+
+	        var resizeWidth = null,
+	            resizeHeight = null,
+	            xpos = editor.offsetLeft + 5,
+	            ypos = editor.offsetTop + 5;
+
+	        resizer.resizable({
+	            aspectRatio: true,
+	            maxWidth: editor.width - 1,
+	            maxHeight: editor.height - 1,
+	            resize: function(e, ui) {
+	                resizeWidth = Math.round(ui.size.width);
+	                resizeHeight = Math.round(ui.size.height);
+	                /** tooltip to show new size */
+	                var string = "New width: " + resizeWidth + "px,<br />new height: " + resizeHeight + "px";
+	                if ($("#tip").length) {
+	                    $("#tip").html(string);
+	                } else {
+	                    var tip = $("<p></p>", {
+	                        id: "tip",
+	                        html: string
+	                    }).css({
+	                        left: xpos,
+	                        top: ypos
+	                    }).appendTo("body");
+	                }
+	            },
+	            stop: function(e, ui) {
+	                /** confirm resize, then do it */
+	                var confirmDialog = $("<div></div>", {
+	                    html: "Image will be resized to " + resizeWidth + "px wide, and " + resizeHeight + "px high.<br />Proceed?"
+	                });
+	                /** init confirm dialog */
+	                confirmDialog.dialog({
+	                    resizable: false,
+	                    modal: true,
+	                    title: "Confirm resize?",
+	                    buttons: {
+	                        Cancel: function() {
+	                            /** tidy up */
+	                            $(this).dialog("close");
+	                            resizer.remove();
+	                            $("#tip").remove();
+	                        },
+	                    Yes: function() {
+	                        /** tidy up */
+	                        $(this).dialog("close");
+	                        resizer.remove();
+	                        $("#tip").remove();
+	                        $("<img/>", {
+	                            src: editor.toDataURL(),
+	                            load: function() {
+	                                /** remove old image */
+	                                context.clearRect(0, 0, editor.width, editor.height);
+	                                /** resize canvas */
+	                                editor.width = resizeWidth;
+	                                editor.height = resizeHeight;
+	                                /** redraw saved image */
+	                                context.drawImage(this, 0, 0, resizeWidth, resizeHeight);
+	                            }
+	                        });
+	                    }
+	                }
+	            });
+	        }
+	    });
 	}
 };
 
